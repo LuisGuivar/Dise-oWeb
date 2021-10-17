113bf43f42b476fbba38e999e90a0c6b6e361fad
LGUIVAR <luisguivarsantacruz@gmail.com>
2021-10-17 02:11:48 -0500

    RESPALDO11

--- SESION07/js/mijavascript.js
+++ SESION07/js/mijavascript.js
@@ -1,3 +1,44 @@
+function Sumatoria(){
+    var sumatoria =0;
+    var contador =0;
+    while(contador <=10){
+        sumatoria += contador;
+        contador++;
+        alert(sumatoria);
+        
+    }
+   
+}
+function Sumatoriadepares(){
+    var sumatoria =0;
+       for(var i=0; i<10; i++){
+           if(i % 2 == 0){
+               sumatoria+=i;
+            
+           }
+           
+       }
+       alert("La sumatoria de numeros pares entre 0 y 10 es:" + sumatoria)
+      
+       
+    }
+   
+function PedirNumeros(){
+   
+        var numeroGuardado = [];
+        var msg = 'Multiplicamos los números elejidos por 3:\n';
+        for(i=0; i<5; i++){
+            numeroPedido = prompt('Ingrese un número');
+            numeroGuardado[i] = Number(numeroPedido);
+        }
+        for (i in numeroGuardado){
+            msg = msg + numeroGuardado[i] + '*3 = ' + (numeroGuardado[i] * 3) + '\n';
+        }
+        alert(msg);
+   
+}
+
+
 function compara(){
     var a =2;
     var b =2;
@@ -18,7 +59,7 @@ document.getElementById("sumar").innerHTML="La suma es " + suma
 }
 
 function escribir(){
-    calor=document.getElementById('entrada').value;
+    valor=document.getElementById('entrada').value;
     document.getElementById('contenido').innerHTML=''+valor;
 }
 
